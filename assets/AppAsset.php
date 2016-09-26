ce6f068936811dc1766c4064095f73f07831e548
Cang Ta <hoksilato176@gmail.com>
2016-09-26 23:16:58 +0700

    move css, js and img folders into themes's assets folder to prevent publishing views folder to web/assets

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -15,8 +15,7 @@ use yii\web\AssetBundle;
  */
 class AppAsset extends AssetBundle
 {
-    public $basePath = '@webroot';
-    public $baseUrl = '@web';
+    public $sourcePath = '@app/themes/default/assets';
     public $css = [
         'css/site.css',
     ];
